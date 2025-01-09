320de7b7db04bd1ba1e74db9d9106a3971073ee8
Theophilus Eriata <theoeriata@gmail.com>
2025-01-09 00:54:01 -0700

    ThirdParty: Add SDL Configuration To Build System.
    * TODO: Disable unused functionality to save compilation.

---
+++ init.cmake
@@ -0,0 +1,6 @@
+######################################
+### Simple Directmedia Layer Support
+######################################
+
+set (XII_3RDPARTY_SDL_SUPPORT ON CACHE BOOL "Use Simple Directmedia Layer (SDL) to manage OS windows and input.")
+mark_as_advanced(FORCE XII_3RDPARTY_SDL_SUPPORT)
